9ba15770b5d7cfced0ebf09aff33404ebe1e088f
Margarita <altsybeevam@yandex.ru>
2024-03-06 03:00:56 +0300

    fix api

--- public/modules/api.js
+++ public/modules/api.js
@@ -1,7 +1,7 @@
 import { getRequest, postRequest } from './ajax.js';
 
-// const BASE_URL = 'http://5.35.16.157/api';
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'http://5.35.16.157/api';
+// const BASE_URL = 'http://localhost:8080/api';
 
 const API_CONST = {
   LOGIN: `${BASE_URL}/auth/login`,
