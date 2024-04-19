2dc63a654aab5e43b54acd6fdc761473166cdd07
Margarita <altsybeevam@yandex.ru>
2024-04-19 18:37:41 +0300

    fix render error page

--- public/views/errorView.js
+++ public/views/errorView.js
@@ -1,8 +1,11 @@
 import { ErrorPage } from '@pages';
+import { authModel } from '@models';
+import { router } from '@modules';
 
 export class ErrorView {
   constructor() {
     this.parent = 'app';
+    authModel.addObserver(this);
     this.errorPage = new ErrorPage(this.parent, {});
   }
 
@@ -11,6 +14,15 @@ export class ErrorView {
    */
   render() {
     this.errorPage.renderAndDidMount();
+  }
+
+  update(event) {
+    if (event.name === 'AUTH') {
+      if (event.data !== true) {
+        return;
+      }
+      router.go(window.location.pathname);
+    }
   }
 
   /**
