60f5d688f2483ccf2ec833a1cdab1af8493061a1
Margarita <altsybeevam@yandex.ru>
2024-04-27 14:32:13 +0300

    page from iframe

--- public/index.js
+++ public/index.js
@@ -1,11 +1,6 @@
 import { router } from '@modules';
 import './index.scss';
 import {
-  AdvertView, ComplexView,
-  MainView, ProfileView,
-  NewAdvertView, MyAdvertView,
-  EditAdvertView, NavbarView,
-  ErrorView, MobileView,
   CsatView,
 } from '@views';
 import { authModel } from '@models';
@@ -18,15 +13,6 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-const navbar = new NavbarView();
-navbar.render();
-router.register('/', MainView);
-router.register('/profile/', ProfileView, true);
-router.register('/adverts/', AdvertView);
-router.register('/complex/', ComplexView);
-router.register('/new-advert/', NewAdvertView, true);
-router.register('/my-advert/', MyAdvertView, true);
-router.register('/edit-advert/', EditAdvertView, true);
 router.register('/csat/', CsatView);
 router.start();
 document.addEventListener('DOMContentLoaded', authModel.checkAuthentication.bind(authModel));
